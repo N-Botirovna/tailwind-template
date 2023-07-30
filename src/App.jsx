d2038ceebc509922929854d5dd4db35e00f2e4f8
N-Botirovna <nargizatosheva20@gmail.com>
2023-07-30 23:22:02 +0300

    add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <h1 className="text-red-600">Hello</h1>
+    </>
+  );
 }
 
 export default App;
